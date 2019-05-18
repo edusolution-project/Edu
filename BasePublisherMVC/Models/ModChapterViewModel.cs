6b75f96fc269f5404c2962fb03405b12c77d12a6
VOGDELL\VOG <vietphung.it@gmail.com>
2019-05-18 10:59:37 +0700

    Add ViewModel for chapter & course

---
+++ BasePublisherMVC/Models/ModChapterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CoreMongoDB.Repositories;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using BasePublisherModels.Database;
+
+namespace BasePublisherMVC.ViewModel
+{
+    public class ModChapterViewModel : ModChapterEntity
+    {
+        public ModChapterEntity Parent { get; set; }
+
+        public ModChapterViewModel(ModChapterEntity entity)
+        {
+            ID = entity.ID;
+            ParentID = entity.ParentID;
+            IsActive = entity.IsActive;
+            Code = entity.Code;
+            CreateUser = entity.CreateUser;
+            Name = entity.Name;
+            Parent = null;
+            Created = entity.Created;
+            Updated = entity.Updated;
+            Order = entity.Order;
+            CourseID = entity.CourseID;
+        }
+
+    }
+}
