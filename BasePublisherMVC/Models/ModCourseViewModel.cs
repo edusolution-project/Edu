a0efcb180229b4b423c11570e12f1ad63b0f9c44
vietphungit <vietphung.it@gmail.com>
2019-05-18 08:57:15 +0700

    u[date

---
+++ BasePublisherMVC/Models/ModCourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BasePublisherModels.Database;
+
+namespace BasePublisherMVC.AdminControllers
+{
+    internal class ModCourseViewModel
+    {
+        private ModCourseEntity t;
+
+        public ModCourseViewModel(ModCourseEntity t)
+        {
+            this.t = t;
+        }
+
+        public ModProgramEntity Program { get; set; }
+        public ModGradeEntity Grade { get; set; }
+        public ModSubjectEntity Subject { get; set; }
+        public int ChildNodeCount { get; set; }
+    }
+}
