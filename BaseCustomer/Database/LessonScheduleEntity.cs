70d79b103f2b0e53aaf46d86c3979142675a42a6
vietphungit <vietphung.it@gmail.com>
2019-07-01 12:03:53 +0700

    Fix Giáo trình + Tạo lớp học

---
+++ BaseCustomer/Database/LessonScheduleEntity.cs
@@ -0,0 +1,27 @@
+﻿using Core_v2.Repositories;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System;
+
+namespace BaseCustomerEntity.Database
+{
+    public class LessonScheduleEntity : EntityBase
+    {
+        [JsonProperty("LessonID")]
+        public DateTime LessonID { get; set; }
+        [JsonProperty("OpeningDate")]
+        public DateTime OpeningDate { get; set; }
+        [JsonProperty("CloseDate")]
+        public DateTime CloseDate { get; set; }
+        [JsonProperty("ClassID")]
+        public DateTime ClassID { get; set; }
+    }
+    public class LessonScheduleService : ServiceBase<LessonEntity>
+    {
+        public LessonScheduleService(IConfiguration config) : base(config)
+        {
+
+        }
+    }
+}
---
+++ BaseCustomerMVC/Controllers/Teacher/LessonScheduleController.cs
@@ -0,0 +1,13 @@
+﻿using BaseCustomerMVC.Globals;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseCustomerMVC.Controllers.Teacher
+{
+    public class LessonScheduleController : TeacherController
+    {
+
+    }
+}
