139e0e6eb1fd9a2bae0a7757d652ce9f6698abe0
long <long@DESKTOP-2BN0T5E>
2019-07-09 20:25:41 +0700

    update core mapping

--- Core_v2/Globals/MappingEntity.cs
+++ Core_v2/Globals/MappingEntity.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Core_v2.Globals
 {
@@ -29,8 +30,7 @@ namespace Core_v2.Globals
                 if (newProps.Contains(item))
                 {
                     var value = item.GetValue(oldItem);
-                    var type = item.GetType();
-                    if ((newItem[item.Name] == null) && value != null)
+                    if (Verify(newItem[item.Name]) && value != null)
                     {
                         newItem[item.Name] = value;
                     }
@@ -51,11 +51,10 @@ namespace Core_v2.Globals
             for (int i = 0; oldProps != null && i < oldProps.Count - 1; i++)
             {
                 var item = newProps[i];
-                var type = item.GetType();
                 if (newProps.Contains(item))
                 {
                     var value = item.GetValue(oldItem);
-                    if ((newItem[item.Name] == null || item.Name == "IsActive") && value != null)
+                    if (Verify(newItem[item.Name] == null) && value != null)
                     {
                         newItem[item.Name] = value;
                     }
@@ -80,7 +79,7 @@ namespace Core_v2.Globals
                 if (newProps.Contains(item))
                 {
                     var value = item.GetValue(oldItem);
-                    if ((newItem[item.Name] == null || item.Name == "IsActive") && value != null)
+                    if (Verify(newItem[item.Name]) && value != null)
                     {
                         newItem[item.Name] = value;
                     }
@@ -104,13 +103,74 @@ namespace Core_v2.Globals
                 if (item.Name == "ID" || item.Name == "id" || item.Name == "_id") continue;
                 if (newProps.Contains(item)) {
                     var value = item.GetValue(oldItem);
-                    if ((newItem[item.Name] == null || item.Name == "IsActive") && value != null)
+                    if (Verify(newItem[item.Name]) && value != null)
                     {
                         newItem[item.Name] = value;
                     }
                 }
             }
             return newItem;
+        }
+
+        private bool Verify(object value)
+        {
+            try
+            {
+                if (value == null) return true;
+                if (IsBoolean(value))
+                {
+                    return !(bool)value;
+                }
+                if (IsDateTime(value))
+                {
+                    return (DateTime) value <= DateTime.MinValue;
+                }
+                if (IsNumber(value)) return (int)value == 0;
+
+                return false;
+
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+        }
+        private bool IsBoolean(object value)
+        {
+            try
+            {
+                bool x;
+                return bool.TryParse(value.ToString(), out x);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private bool IsDateTime(object value)
+        {
+            try
+            {
+                DateTime x;
+                return DateTime.TryParse(value.ToString(), out x);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private bool IsNumber(object value)
+        {
+            try
+            {
+                string pText = value.ToString();
+                Regex regex = new Regex(@"^[-+]?[0-9]*\.?[0-9]+$");
+                return regex.IsMatch(pText);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
