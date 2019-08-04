f7101db1cc694570771428263761b579331c73cc
long <longthaihoang94@gmail.com>
2019-08-04 13:26:25 +0700

    update myhub

---
+++ BaseHub/Database/ChatEntity.cs
@@ -0,0 +1,36 @@
+﻿using Core_v2.Repositories;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseHub.Database
+{
+    public class ChatEntity : EntityBase
+    {
+        [JsonProperty("Sender")]
+        public string Sender { get; set; }
+        [JsonProperty("Reciever")]
+        public string Reciever { get; set; }
+        [JsonProperty("IsGroup")]
+        public bool IsGroup { get; set; } // nếu là group reciever => là group name
+        [JsonProperty("Content")]
+        public string Content { get; set; }
+        [JsonProperty("Medias")]
+        public List<MediaModel> Medias { get; set; }
+        [JsonProperty("Time")]
+        public DateTime Time { get; set; }
+        [JsonProperty("UserView")]
+        public string UserView { get; set; }
+        [JsonProperty("Tags")]
+        public List<string> Tags { get; set; }
+    }
+    public class ChatService : ServiceBase<ChatEntity>
+    {
+        public ChatService(IConfiguration config) : base(config)
+        {
+
+        }
+    }
+}
---
+++ BaseHub/Database/CommentEntity.cs
@@ -0,0 +1,29 @@
+﻿using Core_v2.Repositories;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseHub.Database
+{
+    public class CommentEntity : EntityBase
+    {
+        [JsonProperty("NewID")]
+        public string NewID { get; set; }
+        [JsonProperty("ParentID")]
+        public string ParentID { get; set; }
+        [JsonProperty("Sender")]
+        public string Sender { get; set; }
+        [JsonProperty("Time")]
+        public DateTime Time { get; set; }
+        [JsonProperty("Content")]
+        public string Content { get; set; }
+        [JsonProperty("Medias")]
+        public List<MediaModel> Medias { get; set; }
+        [JsonProperty("Likes")]
+        public List<string> Likes { get; set; }
+        [JsonProperty("Tags")]
+        public List<string> Tags { get; set; }
+
+    }
+}
