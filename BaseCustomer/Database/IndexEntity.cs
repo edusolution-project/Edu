5e47cd6389b6bb9efac0e29db333ebb6bf8484cb
VOGDELL\VOG <vietphung.it@gmail.com>
2019-08-13 23:03:30 +0700

    Canvas Home + My Course + Schedule

---
+++ BaseCustomer/Database/IndexEntity.cs
@@ -0,0 +1,26 @@
+﻿using Core_v2.Repositories;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB;
+
+namespace BaseCustomerEntity.Database
+{
+    public class IndexEntity : EntityBase
+    {        public string key { get; set; }
+        public int index { get; set; } = 0;
+    }
+    public class IndexService : ServiceBase<IndexEntity>
+    {
+        public IndexService(IConfiguration config) : base(config)
+        {
+        }
+        public int GetNewIndex(string name)
+        {
+            return Collection.FindOneAndUpdate(o => o.key == name, Builders<IndexEntity>.Update.Inc("index", 1)).index;
+        }
+    }
+}
