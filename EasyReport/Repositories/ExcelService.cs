7cfeea03c0a12bd600a7ca6b10baff753bf7c7ff
longht <longthaihoang94@gmail.com>
2020-04-08 22:05:03 +0700

    zoom update

--- EasyReport/Repositories/ExcelService.cs
+++ EasyReport/Repositories/ExcelService.cs
@@ -12,10 +12,16 @@ namespace EasyReport.Repositories
         {
 
         }
+
+        private void ImportTemplate(string file)
+        {
+
+        }
+
         private void export(string sheetName)
         {
             // tạo mới 1 workbook
-            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFWorkbook workbook = new XSSFWorkbook("EventExpenseReport.xls");
             // tạo sheet
             ISheet sheet = workbook.CreateSheet(sheetName);
 
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.Converter;
+using System;
+using System.IO;
 
 namespace Test
 {
@@ -7,8 +10,28 @@ namespace Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var a = new EasyReport.Test();
-            a.Excel();
+            //var a = new EasyReport.Test();
+            //FileStream fs = new FileStream("C:\\Users\\longht\\source\\repos\\edusolution-project\\Edu\\Test\\EventExpenseReport.xls", FileMode.Open, FileAccess.Read);
+            //HSSFWorkbook templateWorkbook = new HSSFWorkbook(fs,true);
+            //var sheet = templateWorkbook.GetSheet("Event Budget");
+            //var header = sheet.Header;
+            //var footer = sheet.Footer;
+
+            string fileName = "EventExpenseReport.xls";
+            fileName = Path.Combine(Environment.CurrentDirectory, fileName);
+            HSSFWorkbook workbook = ExcelToHtmlUtils.LoadXls(fileName);
+
+
+            ExcelToHtmlConverter excelToHtmlConverter = new ExcelToHtmlConverter();
+
+            // Process the Excel file
+            excelToHtmlConverter.ProcessWorkbook(workbook);
+
+            var doc = excelToHtmlConverter.Document;
+
+            var path = Path.ChangeExtension(fileName, "html");
+            // Output the HTML file
+            excelToHtmlConverter.Document.Save(path);
         }
     }
 }
