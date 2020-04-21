4f7b1e95c9b094a2bfc2a421d63f8b202364cf91
longht <longthaihoang94@gmail.com>
2020-04-22 00:08:12 +0700

    update

--- BaseCustomerMVC/Controllers/Admin/RoleController.cs
+++ BaseCustomerMVC/Controllers/Admin/RoleController.cs
@@ -25,13 +25,11 @@ namespace BaseCustomerMVC.Controllers.Admin
     public class RoleController : AdminController
     {
         private readonly ILog _log;
-        private readonly IAccess _access;
-        private readonly AccessesService _accessesService;
         private readonly RoleService _service;
-        private readonly IHostingEnvironment _env;
-        public RoleController(ILog log)
+        public RoleController(ILog log, RoleService service)
         {
             _log = log;
+            _service = service;
         }
         // GET: Home
 
@@ -207,91 +205,6 @@ namespace BaseCustomerMVC.Controllers.Admin
                 return new JsonResult(new { code = 500, msg = ex.Message, data = ex });
             }
         }
-
-
-        public IActionResult Detail(string id)
-        {
-            if (string.IsNullOrEmpty(id)) return View("Index");
-
-            var assembly = GetAssembly();
-
-            ViewBag.AdminCtrl = _access.GetAccessByAttribue<Globals.AdminController>(assembly, "admin");
-            ViewBag.TeacherCtrl = _access.GetAccessByAttribue<Globals.TeacherController>(assembly, "teacher");
-            ViewBag.StudentCtrl = _access.GetAccessByAttribue<Globals.StudentController>(assembly, "student");
-
-            ViewBag.Data = _accessesService.Collection.Find(o => o.RoleID == id && o.IsActive == true)?.ToList();
-            ViewBag.RoleID = id;
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Detail(string id, List<AccessEntity> data)
-        {
-            if (string.IsNullOrEmpty(id)) return View("Index");
-
-            var assembly = GetAssembly();
-
-            ViewBag.AdminCtrl = _access.GetAccessByAttribue<Globals.AdminController>(assembly, "admin");
-            ViewBag.TeacherCtrl = _access.GetAccessByAttribue<Globals.TeacherController>(assembly, "teacher");
-            ViewBag.StudentCtrl = _access.GetAccessByAttribue<Globals.StudentController>(assembly, "student");
-
-            for (int i = 0; data != null && i < data.Count; i++)
-            {
-                var item = data[i];
-                var oldItem = _accessesService.Collection.Find(o => o.CtrlName == item.CtrlName && o.ActName == item.ActName && o.RoleID == id && o.Type == item.Type)?.SingleOrDefault();
-                if (oldItem != null)
-                {
-                    oldItem.IsActive = item.IsActive;
-                    _accessesService.Collection.ReplaceOne(x => x.ID == oldItem.ID, oldItem);
-                }
-                else
-                {
-                    item.RoleID = id;
-                    item.CreateDate = DateTime.Now;
-                    item.UserCreate = User.FindFirst("UserID")?.Value;
-                    _accessesService.CreateOrUpdate(item);
-                }
-            }
-            ViewBag.Data = _accessesService.Collection.Find(o => o.RoleID == id && o.IsActive == true)?.ToList();
-            ViewBag.RoleID = id;
-            return View();
-        }
-
-        [Obsolete]
-        [HttpPost]
-        public JsonResult GetList(DefaultModel model)
-        {
-            var filter = new List<FilterDefinition<RoleEntity>>();
-
-            if (!string.IsNullOrEmpty(model.SearchText))
-            {
-                filter.Add(Builders<RoleEntity>.Filter.Where(o => o.Name.ToLower().Contains(model.SearchText.ToLower()) || o.Code.ToLower().Contains(model.SearchText.ToLower())));
-            }
-            if (model.StartDate > DateTime.MinValue)
-            {
-                filter.Add(Builders<RoleEntity>.Filter.Where(o => o.CreateDate >= new DateTime(model.StartDate.Year, model.StartDate.Month, model.StartDate.Day, 0, 0, 0)));
-            }
-            if (model.EndDate > DateTime.MinValue)
-            {
-                filter.Add(Builders<RoleEntity>.Filter.Where(o => o.CreateDate <= new DateTime(model.EndDate.Year, model.EndDate.Month, model.EndDate.Day, 23, 59, 59)));
-            }
-            var data = filter.Count > 0 ? _service.Collection.Find(Builders<RoleEntity>.Filter.And(filter)) : _service.GetAll();
-            model.TotalRecord = data.Count();
-            var DataResponse = data == null || data.Count() <= 0 || data.Count() < model.PageSize
-                ? data.ToList()
-                : data.Skip((model.PageIndex - 1) * model.PageSize).Limit(model.PageSize).ToList();
-            var response = new Dictionary<string, object>
-            {
-                { "Data", DataResponse },
-                { "Model", model }
-            };
-            return new JsonResult(response);
-
-        }
-
-
-        private Assembly GetAssembly()
-        {
-            return Assembly.GetExecutingAssembly();
-        }
+       
     }
 }
