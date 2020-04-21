59b77fa291bd66b94725c428554fb44851839373
longht <longthaihoang94@gmail.com>
2020-04-22 00:05:09 +0700

    update role

--- BaseCustomerMVC/Controllers/Admin/RoleController.cs
+++ BaseCustomerMVC/Controllers/Admin/RoleController.cs
@@ -15,25 +15,23 @@ using System.Linq;
 using MongoDB.Bson;
 using BaseAccess.Interfaces;
 using System.Reflection;
+using Core_v2.Interfaces;
+using BaseCustomerEntity.Globals;
+using System.Security.Claims;
 
 namespace BaseCustomerMVC.Controllers.Admin
 {
     [BaseAccess.Attribule.AccessCtrl("Quản lý quyền", "admin", 4)]
     public class RoleController : AdminController
     {
+        private readonly ILog _log;
         private readonly IAccess _access;
         private readonly AccessesService _accessesService;
         private readonly RoleService _service;
         private readonly IHostingEnvironment _env;
-        public RoleController(RoleService service
-            , AccessesService accessesService
-            , IAccess access
-            , IHostingEnvironment evn)
+        public RoleController(ILog log)
         {
-            _env = evn;
-            _service = service;
-            _access = access;
-            _accessesService = accessesService;
+            _log = log;
         }
         // GET: Home
 
@@ -41,6 +39,173 @@ namespace BaseCustomerMVC.Controllers.Admin
         {
             ViewBag.Model = model;
             return View();
+        }
+        [HttpGet]
+        public JsonResult Get(DefaultModel model)
+        {
+            try
+            {
+                if (User != null && User.Identity.IsAuthenticated)
+                {
+                    string parent = User.FindFirst(ClaimTypes.Role)?.Value;
+                    if (parent != null && parent != "supperadmin")
+                    {
+                        var data = _service.CreateQuery().Find(o =>
+                        (string.IsNullOrEmpty(model.SearchText) || (o.Name.Contains(model.SearchText) || o.Code.Contains(model.SearchText)) &&
+                        (string.IsNullOrEmpty(model.ID) || o.ID == model.ID) &&
+                        (parent != null && o.ParentID == parent) // chỉ lấy data từ thằng cha
+                        ))?.ToList();
+                        if (data != null)
+                        {
+                            return new JsonResult(new { code = 200, msg = "success", data = data });
+                        }
+                    }
+                    if(parent == "supperadmin")
+                    {
+                        var data = _service.CreateQuery().Find(o =>
+                        (string.IsNullOrEmpty(model.SearchText) || (o.Name.Contains(model.SearchText) || o.Code.Contains(model.SearchText)) &&
+                        (string.IsNullOrEmpty(model.ID) || o.ID == model.ID)))?.ToList();
+
+                        if (data != null)
+                        {
+                            return new JsonResult(new { code = 200, msg = "success", data = data });
+                        }
+                    }
+                }
+                return new JsonResult(new { code = 404, msg = "Data not found" });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { code = 500, msg = ex.Message, data = ex });
+            }
+        }
+        [HttpPost]
+        public JsonResult Create(RoleEntity item)
+        {
+            try
+            {
+                if(User != null && User.Identity.IsAuthenticated)
+                {
+                    if (!string.IsNullOrEmpty(item.Name))
+                    {
+                        item.UserCreate = User.FindFirst("UserID")?.Value;
+                        item.CreateDate = DateTime.Now;
+                        string code = item.Name.ConvertUnicodeToCode("", true);
+                        item.Code = code;
+                        if (_service.GetItemByCode(code) == null)
+                        {
+                            _service.CreateOrUpdate(item);
+                            return new JsonResult(new { code = 200, msg = "create success", data = item });
+                        }
+                        else
+                        {
+                            return new JsonResult(new { code = 301, msg = "exist item", data = item });
+                        }
+                    }
+                    return new JsonResult(new { code = 305, msg = "name empty", data = item });
+                }
+                return new JsonResult(new { code = 405, msg = "User not found", data = item });
+            }
+            catch(Exception ex)
+            {
+                return new JsonResult(new { code = 500, msg = ex.Message, data = ex });
+            }
+        }
+        [HttpDelete]
+        public JsonResult Delete(string id)
+        {
+            try
+            {
+                if (User != null && User.Identity.IsAuthenticated)
+                {
+                    if (!string.IsNullOrEmpty(id))
+                    {
+                        var item = _service.GetItemByID(id);
+                        if(item == null)
+                        {
+                            return new JsonResult(new { code = 404, msg = "Data not found" });
+                        }
+                        if(item.Code == User.FindFirst(ClaimTypes.Role)?.Value || item.Code == "superadmin")
+                        {
+                            return new JsonResult(new { code = 405, msg = "Insufficient authority" });
+                        }
+                        _service.Remove(id);
+                        return new JsonResult(new { code = 200, msg = "delete success", data = item });
+                    }
+                    return new JsonResult(new { code = 305, msg = "id empty"});
+                }
+                return new JsonResult(new { code = 404, msg = "Data not found" });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { code = 500, msg = ex.Message, data = ex });
+            }
+        }
+        [HttpPut]
+        public JsonResult Update(RoleEntity item)
+        {
+            try
+            {
+                if (User != null && User.Identity.IsAuthenticated)
+                {
+                    if (item != null && !string.IsNullOrEmpty(item.ID))
+                    {
+                        var oldItem = _service.GetItemByID(item.ID);
+                        if (oldItem != null)
+                        {
+                            if (item.Code == User.FindFirst(ClaimTypes.Role)?.Value || item.Code == "superadmin")
+                            {
+                                return new JsonResult(new { code = 405, msg = "Insufficient authority" });
+                            }
+                            item.Code = oldItem.Code;
+                            item.UserCreate = oldItem.UserCreate;
+                            item.CreateDate = oldItem.CreateDate;
+                            if (string.IsNullOrEmpty(item.ParentID)) item.ParentID = oldItem.ParentID;
+                            _service.CreateOrUpdate(item);
+                            return new JsonResult(new { code = 200, msg = "update success", data = item });
+                        }
+                    }
+                    return new JsonResult(new { code = 404, msg = "Data not found" });
+                }
+                return new JsonResult(new { code = 404, msg = "Data not found" });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { code = 500, msg = ex.Message, data = ex });
+            }
+        }
+
+        [HttpPut]
+        public JsonResult Approved(string id)
+        {
+            try
+            {
+                if (User != null && User.Identity.IsAuthenticated)
+                {
+                    if (!string.IsNullOrEmpty(id))
+                    {
+                        var item = _service.GetItemByID(id);
+                        if (item == null)
+                        {
+                            return new JsonResult(new { code = 404, msg = "Data not found" });
+                        }
+                        if (item.Code == User.FindFirst(ClaimTypes.Role)?.Value || item.Code == "superadmin")
+                        {
+                            return new JsonResult(new { code = 405, msg = "Insufficient authority" });
+                        }
+                        var filter = Builders<RoleEntity>.Filter.Eq(o=>o.ID, item.ID);
+                        var update = Builders<RoleEntity>.Update.Set(o => o.IsActive, !item.IsActive);
+                        _service.CreateQuery().UpdateOne(filter, update);
+                        return new JsonResult(new { code = 200, msg = "Approved success", data = item });
+                    }
+                    return new JsonResult(new { code = 305, msg = "id empty" });
+                }
+                return new JsonResult(new { code = 404, msg = "Data not found" });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { code = 500, msg = ex.Message, data = ex });
+            }
         }
 
 
