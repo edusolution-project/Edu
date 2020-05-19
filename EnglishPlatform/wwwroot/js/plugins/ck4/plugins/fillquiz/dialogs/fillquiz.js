7a6a802ba4edbba66efe9b520330ee1d8ae927a7
vietphungit <vietphung.it@gmail.com>
2020-05-19 12:16:21 +0700

    Fill Quiz Cont

--- EnglishPlatform/wwwroot/js/plugins/ck4/plugins/fillquiz/dialogs/fillquiz.js
+++ EnglishPlatform/wwwroot/js/plugins/ck4/plugins/fillquiz/dialogs/fillquiz.js
@@ -4,155 +4,177 @@
 
 // Our dialog definition.
 CKEDITOR.dialog.add('fillquizDialog', function (editor) {
-	return {
+    return {
 
-		// Basic properties of the dialog window: title, minimum size.
-		title: 'Fillquiz Properties',
-		minWidth: 400,
-		minHeight: 200,
+        // Basic properties of the dialog window: title, minimum size.
+        title: 'Fillquiz Properties',
+        minWidth: 400,
+        minHeight: 200,
 
-		// Dialog window content definition.
-		contents: [
-			{
-				// Definition of the Basic Settings dialog tab (page).
-				id: 'tab-fillquiz',
-				label: 'FillQuiz Settings',
+        // Dialog window content definition.
+        contents: [
+            {
+                // Definition of the Basic Settings dialog tab (page).
+                id: 'tab-fillquiz',
+                label: 'FillQuiz Settings',
 
-				// The tab content.
-				elements: [
-					{
-						// Text input field for the Fillquiz Placeholder text.
-						type: 'text',
-						id: 'quizPlc',
-						label: 'Placeholder',
+                // The tab content.
+                elements: [
+                    {
+                        // Text input field for the Fillquiz Placeholder text.
+                        type: 'text',
+                        id: 'quizPlc',
+                        label: 'Placeholder',
 
-						// Validation checking whether the field is not empty.
-						validate: CKEDITOR.dialog.validate.notEmpty("Placeholder field cannot be empty."),
+                        // Validation checking whether the field is not empty.
+                        validate: CKEDITOR.dialog.validate.notEmpty("Placeholder field cannot be empty."),
 
-						// Called by the main setupContent method call on dialog initialization.
-						setup: function (element) {
-							var childInp = element.find("input");
+                        // Called by the main setupContent method call on dialog initialization.
+                        setup: function (element) {
+                            var childInp = element.find("input");
 
-							if (childInp.$.length > 0) {
-								this.setValue(childInp.$[0].getAttribute("value"));
-							}
-							else {
-								this.setValue(element.getText());
-							}
-						},
+                            if (childInp.$.length > 0) {
+                                this.setValue(childInp.$[0].getAttribute("plc"));
+                            }
+                            else {
+                                this.setValue(element.getText());
+                            }
+                        },
 
-						// Called by the main commitContent method call on dialog confirmation.
-						commit: function (element) {
-							var childInp = element.find("input");
-							if (childInp.$.length == 0) {
-								element.setText("");
-								var childInp = editor.document.createElement('input');
-								childInp.setAttribute("type", "text");
-								childInp.setAttribute("value", this.getValue());
-								childInp.setAttribute("disabled", "disabled");
-								childInp.setAttribute("class", "fillquiz");
-								element.append(childInp);
-							}
-							else
-								childInp[0].setText(this.getValue());
-						}
-					},
-					{
-						// Text input field for the Fillquiz text.
-						type: 'text',
-						id: 'quizAnswer',
-						label: 'Answer',
+                        // Called by the main commitContent method call on dialog confirmation.
+                        commit: function (element) {
+                            var childInp = element.find("input");
+                            if (childInp.$.length == 0) {
+                                element.setText("");
+                                var childInp = editor.document.createElement('input');
+                                childInp.setAttribute("type", "text");
+                                childInp.setAttribute("value", this.getValue());
+                                childInp.setAttribute("disabled", "disabled");
+                                childInp.setAttribute("class", "fillquiz");
+                                element.append(childInp);
+                            }
+                            else {
+                                childInp.$[0].setAttribute("plc", this.getValue());
+                                childInp.$[0].setAttribute("value", this.getValue());
+                            }
+                        }
+                    },
+                    {
+                        // Text input field for the Fillquiz text.
+                        type: 'text',
+                        id: 'quizAnswer',
+                        label: 'Answer',
 
-						// Validation checking whether the field is not empty.
-						validate: CKEDITOR.dialog.validate.notEmpty("Answer field cannot be empty."),
+                        // Validation checking whether the field is not empty.
+                        validate: CKEDITOR.dialog.validate.notEmpty("Answer field cannot be empty."),
 
-						// Called by the main setupContent method call on dialog initialization.
-						setup: function (element) {
-							var label = element.find("label");
-							if (label.$.length > 0) {
-								this.setValue(label[0].getText());
-							}
-							else {
-								this.setValue(element.getAttribute("ans"));
-							}
-						},
+                        // Called by the main setupContent method call on dialog initialization.
+                        setup: function (element) {
+                            this.setValue(element.getAttribute("ans"));
+                        },
 
-						// Called by the main commitContent method call on dialog confirmation.
-						commit: function (element) {
-							element.setAttribute("ans", this.getValue());
-						}
-					},
-					{
-						// Text input field for the Fillquiz title (explanation).
-						type: 'text',
-						id: 'quizExp',
-						label: 'Explanation',
-						validate: CKEDITOR.dialog.validate.notEmpty("Explanation field cannot be empty."),
+                        // Called by the main commitContent method call on dialog confirmation.
+                        commit: function (element) {
+                            var childInp = element.find("input");
+                            if (childInp.$.length == 0) {
+                                element.setText("");
+                                var childInp = editor.document.createElement('input');
+                                childInp.setAttribute("type", "text");
+                                childInp.setAttribute("ans", this.getValue());
+                                var text = "(" + this.getValue() + ")";
+                                var i = text.length;
+                                var space = 0;
+                                while (i--) { if (text.charAt(i) == ' ') space++; }
 
-						// Called by the main setupContent method call on dialog initialization.
-						setup: function (element) {
-							this.setValue(element.getAttribute("title"));
-						},
+                                childInp.$[0].setAttribute("value", text);
+                                childInp.$[0].setAttribute("size", text.length - space);
+                                childInp.setAttribute("disabled", "disabled");
+                                childInp.setAttribute("class", "fillquiz");
+                                element.append(childInp);
+                            }
+                            else {
+                                childInp.$[0].setAttribute("ans", this.getValue());
+                                var text = childInp.$[0].getAttribute("value") + " (" + this.getValue() + ")";
+                                var i = text.length;
+                                var space = 0;
+                                while (i--) { if (text.charAt(i) == ' ') space++; }
 
-						// Called by the main commitContent method call on dialog confirmation.
-						commit: function (element) {
-							element.setAttribute("title", this.getValue());
-						}
-					}
-				]
-			}
-		],
+                                childInp.$[0].setAttribute("value", text);
+                                childInp.$[0].setAttribute("size", text.length - space);
+                            }
+                        }
+                    },
+                    {
+                        // Text input field for the Fillquiz title (explanation).
+                        type: 'text',
+                        id: 'quizExp',
+                        label: 'Explanation',
+                        validate: CKEDITOR.dialog.validate.notEmpty("Explanation field cannot be empty."),
 
-		// Invoked when the dialog is loaded.
-		onShow: function () {
-			// Get the selection from the editor.
-			var selection = editor.getSelection();
+                        // Called by the main setupContent method call on dialog initialization.
+                        setup: function (element) {
+                            this.setValue(element.getAttribute("title"));
+                        },
 
-			// Get the element at the start of the selection.
-			var element = selection.getStartElement();
+                        // Called by the main commitContent method call on dialog confirmation.
+                        commit: function (element) {
+                            element.setAttribute("title", this.getValue());
+                        }
+                    }
+                ]
+            }
+        ],
 
-			// Get the <fillquiz> element closest to the selection, if it exists.
-			if (element)
-				element = element.getAscendant('fillquiz', true);
+        // Invoked when the dialog is loaded.
+        onShow: function () {
+            // Get the selection from the editor.
+            var selection = editor.getSelection();
 
-			// Create a new <fillquiz> element if it does not exist.
-			if (!element || element.getName() != 'fillquiz') {
-				{
-					element = editor.document.createElement('fillquiz');
-				}
+            // Get the element at the start of the selection.
+            var element = selection.getStartElement();
 
-				// Flag the insertion mode for later use.
-				this.insertMode = true;
-			}
-			else {
-				this.insertMode = false;
-			}
+            // Get the <fillquiz> element closest to the selection, if it exists.
+            if (element)
+                element = element.getAscendant('fillquiz', true);
 
-			// Store the reference to the <fillquiz> element in an internal property, for later use.
-			this.element = element;
+            // Create a new <fillquiz> element if it does not exist.
+            if (!element || element.getName() != 'fillquiz') {
+                {
+                    element = editor.document.createElement('fillquiz');
+                }
 
-			// Invoke the setup methods of all dialog window elements, so they can load the element attributes.
-			if (!this.insertMode)
-				this.setupContent(this.element);
-		},
+                // Flag the insertion mode for later use.
+                this.insertMode = true;
+            }
+            else {
+                this.insertMode = false;
+            }
 
-		// This method is invoked once a user clicks the OK button, confirming the dialog.
-		onOk: function () {
-			var dialog = this;
-			// Create a new <fillquiz> element.
-			var fillquiz = this.element;
-			// Invoke the commit methods of all dialog window elements, so the <fillquiz> element gets modified.
-			this.commitContent(fillquiz);
+            // Store the reference to the <fillquiz> element in an internal property, for later use.
+            this.element = element;
 
-			// Finally, if in insert mode, insert the element into the editor at the caret position.
-			if (this.insertMode) {
-				editor.insertElement(fillquiz);
-				//debugger;
+            // Invoke the setup methods of all dialog window elements, so they can load the element attributes.
+            if (!this.insertMode)
+                this.setupContent(this.element);
+        },
 
-				//var quizPlc = dialog.getContentElement('tab-fillquiz', 'quizPlc').getValue();
-				//var quizAnswer = dialog.getContentElement('tab-fillquiz', 'quizAnswer').getValue();
-				//var quizExp = dialog.getContentElement('tab-fillquiz', 'quizExp').getValue();
-			}
-		}
-	};
+        // This method is invoked once a user clicks the OK button, confirming the dialog.
+        onOk: function () {
+            //var dialog = this;
+            // Create a new <fillquiz> element.
+            var fillquiz = this.element;
+            // Invoke the commit methods of all dialog window elements, so the <fillquiz> element gets modified.
+            this.commitContent(fillquiz);
+
+            // Finally, if in insert mode, insert the element into the editor at the caret position.
+            if (this.insertMode) {
+                editor.insertElement(fillquiz);
+                //debugger;
+
+                //var quizPlc = dialog.getContentElement('tab-fillquiz', 'quizPlc').getValue();
+                //var quizAnswer = dialog.getContentElement('tab-fillquiz', 'quizAnswer').getValue();
+                //var quizExp = dialog.getContentElement('tab-fillquiz', 'quizExp').getValue();
+            }
+        }
+    };
 });
