653f6d1b0eadeb8b090d1f2ef4ccc332d9213852
Long Hoang Thai <LongHT@fpts.com.vn>
2020-06-16 15:45:11 +0700

    update . lỗi truy xuất các file và các controller ko có basis

--- EnglishPlatform/Startup.cs
+++ EnglishPlatform/Startup.cs
@@ -10,7 +10,6 @@ using BaseCustomerEntity.Globals;
 using BaseCustomerMVC.Globals;
 using BaseEasyRealTime.Globals;
 using BaseHub;
-using com.wiris.plugin.api;
 using Core_v2.Globals;
 using EasyZoom;
 using EnglishPlatform.Controllers;
@@ -101,6 +100,9 @@ namespace EnglishPlatform
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseEndpointRouting();
+
             app.UseCookiePolicy();
             app.UseSession();
             app.UseAuthentication();
@@ -117,103 +119,99 @@ namespace EnglishPlatform
                     List<AuthorityEntity> data = authorityService.GetAll()?.ToList();
                     CacheExtends.SetObjectFromCache(CacheExtends.DefaultPermission, 3600 * 24 * 360, data);
                 }
-
-                var contextPath = context.Request.Path.Value.ToLower();
-
-                string center = contextPath != "" && contextPath != "/" ? contextPath.Split('/')[1] : string.Empty;
-                if (!string.IsNullOrEmpty(center) &&
-                !center.Contains("hub") &&
-                !center.Contains("files") &&
-                !contextPath.Contains("easyrealtime") && //easyrealtime controller
-                !center.Contains("tin-tuc") &&//news controller
-                !center.Contains("news") &&//news controller
-                !center.Contains("home") &&//home controller
-                !contextPath.Contains("login") &&
-                !contextPath.Contains("logout") &&
-                !(contextPath == "/"))
+                var routingFeature = context.Features[typeof(IRoutingFeature)] as IRoutingFeature;
+                //context.Features[typeof(IEndpointFeature)];
+                RouteData routeValues = routingFeature?.RouteData;
+                if(routeValues != null)
                 {
-                    string userID = context.User.FindFirst("UserID")?.Value;
-                    string roleCode = context.User.FindFirst(System.Security.Claims.ClaimTypes.Role)?.Value;
-                    string type = context.User.FindFirst("Type")?.Value;
-                    if (roleCode != "superadmin")
+                    if (routeValues.Values.Keys.Contains("basis"))
                     {
-                        string key = $"{roleCode}";
-                        string defaultKey = $"{userID}_{center}";
-                        string currentKey = CacheExtends.GetDataFromCache<string>(defaultKey);
-                        if (!string.IsNullOrEmpty(currentKey) && currentKey == key)
+                        string center = routeValues.Values.GetValueOrDefault("basis")?.ToString();
+                        if (!string.IsNullOrEmpty(center))
                         {
-                            // some things
-                        }
-                        else
-                        {
-                            CenterService _centerService = new CenterService(Configuration);
-                            AccountService _accountService = new AccountService(Configuration);
-                            TeacherService _teacherService = new TeacherService(Configuration);
-                            StudentService _studentService = new StudentService(Configuration);
-                            RoleService _roleService = new RoleService(Configuration);
-                            AccessesService _accessesService = new AccessesService(Configuration);
-
-                            string centerCode = center;
-                            var tc = _teacherService.GetItemByID(userID);
-                            var st = _studentService.GetItemByID(userID);
-                            var user = _accountService.GetItemByID(userID);
-                            var defaultUser = new UserModel() { };
-                            bool isRealCenter = false;
-
-                            switch (type)
+                            string userID = context.User.FindFirst("UserID")?.Value;
+                            string roleCode = context.User.FindFirst(System.Security.Claims.ClaimTypes.Role)?.Value;
+                            string type = context.User.FindFirst("Type")?.Value;
+                            if (roleCode != "superadmin")
                             {
-                                case ACCOUNT_TYPE.ADMIN:
-                                    defaultUser = new UserModel(user.ID, "admin");
-                                    centerCode = center;
-                                    roleCode = user.UserName == "supperadmin@gmail.com" ? "superadmin" : "admin";
-                                    break;
-                                case ACCOUNT_TYPE.TEACHER:
-                                    if (tc != null)
-                                    {
-                                        defaultUser = new UserModel(tc.ID, tc.FullName);
-                                        centerCode = string.IsNullOrEmpty(center) && tc.Centers != null && tc.Centers.Count > 0 ? tc.Centers.FirstOrDefault().Code : center;
-                                        roleCode = tc.Centers != null && tc.Centers.Count > 0 ? tc.Centers.FirstOrDefault().RoleID : "";
-                                        isRealCenter = tc.Centers.Any(o => o.Code == centerCode);
-                                    }
-                                    break;
-                                default:
-                                    if (st != null)
-                                    {
-                                        defaultUser = new UserModel(st.ID, st.FullName);
-                                        centerCode = (string.IsNullOrEmpty(center) && st.Centers != null && st.Centers.Count > 0) ? _centerService.GetItemByID(st.Centers.FirstOrDefault()).Code : center;
-                                        roleCode = "student";
-                                        isRealCenter = st.Centers != null && st.Centers.Any(o => o == _centerService.GetItemByCode(centerCode).ID);
-                                        //var avatar = st != null && !string.IsNullOrEmpty(st.Avatar) ? st.Avatar : null;
-                                        //HttpContext.Session.SetString("userAvatar", avatar);
-                                    }
-                                    break;
-                            }
-
-                            if (type != ACCOUNT_TYPE.ADMIN)
-                            {
-                                if (isRealCenter)
+                                string key = $"{roleCode}";
+                                string defaultKey = $"{userID}_{center}";
+                                string currentKey = CacheExtends.GetDataFromCache<string>(defaultKey);
+                                if (!string.IsNullOrEmpty(currentKey) && currentKey == key)
                                 {
-                                    var role = roleCode != "student" ? _roleService.GetItemByID(roleCode) : _roleService.GetItemByCode(roleCode);
-                                    CacheExtends.SetObjectFromCache($"{defaultUser.ID}_{centerCode}", 3600 * 24 * 360, key);
-                                    if (CacheExtends.GetDataFromCache<List<string>>(key) == null)
-                                    {
-                                        var listAccess = _accessesService.GetAccessByRole(role.Code);
-                                        var access = listAccess.Select(o => o.Authority)?.ToList();
-                                        CacheExtends.SetObjectFromCache(key, 3600 * 24 * 360, access);
-                                    }
+                                    // some things
                                 }
                                 else
                                 {
-                                    context.Response.ContentType = "text/html;charset=utf-8";
-                                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                                    await context.Response.WriteAsync("<p>Không tìm thấy thông tin</p>");
-                                    await context.Response.WriteAsync("<a href='/'>quay về</a>");
+                                    CenterService _centerService = new CenterService(Configuration);
+                                    AccountService _accountService = new AccountService(Configuration);
+                                    TeacherService _teacherService = new TeacherService(Configuration);
+                                    StudentService _studentService = new StudentService(Configuration);
+                                    RoleService _roleService = new RoleService(Configuration);
+                                    AccessesService _accessesService = new AccessesService(Configuration);
+
+                                    string centerCode = center;
+                                    var tc = _teacherService.GetItemByID(userID);
+                                    var st = _studentService.GetItemByID(userID);
+                                    var user = _accountService.GetItemByID(userID);
+                                    var defaultUser = new UserModel() { };
+                                    bool isRealCenter = false;
+
+                                    switch (type)
+                                    {
+                                        case ACCOUNT_TYPE.ADMIN:
+                                            defaultUser = new UserModel(user.ID, "admin");
+                                            centerCode = center;
+                                            roleCode = user.UserName == "supperadmin@gmail.com" ? "superadmin" : "admin";
+                                            break;
+                                        case ACCOUNT_TYPE.TEACHER:
+                                            if (tc != null)
+                                            {
+                                                defaultUser = new UserModel(tc.ID, tc.FullName);
+                                                centerCode = string.IsNullOrEmpty(center) && tc.Centers != null && tc.Centers.Count > 0 ? tc.Centers.FirstOrDefault().Code : center;
+                                                roleCode = tc.Centers != null && tc.Centers.Count > 0 ? tc.Centers.FirstOrDefault().RoleID : "";
+                                                isRealCenter = tc.Centers.Any(o => o.Code == centerCode);
+                                            }
+                                            break;
+                                        default:
+                                            if (st != null)
+                                            {
+                                                defaultUser = new UserModel(st.ID, st.FullName);
+                                                centerCode = (string.IsNullOrEmpty(center) && st.Centers != null && st.Centers.Count > 0) ? _centerService.GetItemByID(st.Centers.FirstOrDefault()).Code : center;
+                                                roleCode = "student";
+                                                isRealCenter = st.Centers != null && st.Centers.Any(o => o == _centerService.GetItemByCode(centerCode).ID);
+                                                //var avatar = st != null && !string.IsNullOrEmpty(st.Avatar) ? st.Avatar : null;
+                                                //HttpContext.Session.SetString("userAvatar", avatar);
+                                            }
+                                            break;
+                                    }
+
+                                    if (type != ACCOUNT_TYPE.ADMIN)
+                                    {
+                                        if (isRealCenter)
+                                        {
+                                            var role = roleCode != "student" ? _roleService.GetItemByID(roleCode) : _roleService.GetItemByCode(roleCode);
+                                            CacheExtends.SetObjectFromCache($"{defaultUser.ID}_{centerCode}", 3600 * 24 * 360, key);
+                                            if (CacheExtends.GetDataFromCache<List<string>>(key) == null)
+                                            {
+                                                var listAccess = _accessesService.GetAccessByRole(role.Code);
+                                                var access = listAccess.Select(o => o.Authority)?.ToList();
+                                                CacheExtends.SetObjectFromCache(key, 3600 * 24 * 360, access);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            context.Response.ContentType = "text/html;charset=utf-8";
+                                            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                                            await context.Response.WriteAsync("<p>Không tìm thấy thông tin</p>");
+                                            await context.Response.WriteAsync("<a href='/'>quay về</a>");
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
                 }
-                // Do work that doesn't write to the Response.
                 await next.Invoke();
                 // Do logging or other work that doesn't write to the Response.
             });
