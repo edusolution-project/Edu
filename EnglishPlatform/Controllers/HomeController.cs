6606edf1bc31040937073cb3c56901386bec7afe
VietPhung <vietphung.it@gmail.com>
2020-07-27 14:42:13 +0700

    update

--- EnglishPlatform/Controllers/HomeController.cs
+++ EnglishPlatform/Controllers/HomeController.cs
@@ -648,7 +648,6 @@ namespace EnglishPlatform.Controllers
                 }
                 else
                 {
-                    var teacher = _teacherService.GetItemByID(@event.TeacherID);
                     string zoomId = teacher != null && !string.IsNullOrEmpty(teacher.ZoomID) ? teacher.ZoomID : @event.UrlRoom.Replace("-", "");
                     //ViewBag.Role = "0";
                     ViewBag.Url = Url.Action("ZoomClass", "Home", new { roomID = @event.UrlRoom.Replace("-", "") });
