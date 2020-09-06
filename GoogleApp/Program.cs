90f971e8444510d83584656d241eb7fb7c2159c4
Long Hoàng <longthaihoang94@gmail.com>
2020-09-06 12:22:59 +0700

    update google app api

--- GoogleApp/Program.cs
+++ GoogleApp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using GoogleLib.Interfaces;
+using GoogleLib.Services;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,13 +14,20 @@ namespace GoogleApp
 {
     public class Program
     {
+        public static IGoogleDriveApiService GoogleDriveApiService { get; private set; }
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext, configBuilder) =>
+            {
+                var config = configBuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                if (GoogleDriveApiService == null && config != null)
+                {
+                    GoogleDriveApiService = new GoogleDriveApiService(config.Build());
+                }
+            }).UseStartup<Startup>();
     }
 }
