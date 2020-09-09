77d093e89d4f5d9fd3fac9e3abe6f77e0ae46d96
Hòa Nguyễn <shin.l0v3.ly@gmail.com>
2020-09-09 17:20:39 +0700

    fix đổi password vẫn đăng nhập đc,fix tạo mới bài giảng, update sidebar

--- BaseCustomerMVC/Controllers/Teacher/HomeController.cs
+++ BaseCustomerMVC/Controllers/Teacher/HomeController.cs
@@ -213,6 +213,7 @@ namespace BaseCustomerMVC.Controllers.Teacher
             }
 
             user.PassWord = Core_v2.Globals.Security.Encrypt(newpass);
+            user.PassTemp = Core_v2.Globals.Security.Encrypt(newpass);
             _accountService.CreateOrUpdate(user);
             return new JsonResult(
             new Dictionary<string, object>
