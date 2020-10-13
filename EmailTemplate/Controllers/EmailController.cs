822f8c98bae1462962ef7feb24d9c278f82183c5
VietPhung <vietphung.it@gmail.com>
2020-10-13 17:53:55 +0700

    update

--- EmailTemplate/Controllers/EmailController.cs
+++ EmailTemplate/Controllers/EmailController.cs
@@ -12,15 +12,6 @@ namespace EmailTemplate.Controllers
     public class EmailController : Controller
     {
         private readonly ILogger<EmailController> _logger;
-        //private static ClassService _classService;
-        //private static CenterService _centerService;
-        //private static ClassSubjectService _classSubjectService;
-        //private static LessonService _lessonService;
-        //private static LessonScheduleService _scheduleService;
-        //private static AccountService _accountService;
-        //private static StudentService _studentService;
-        //private static TeacherService _teacherService;
-        //private static SkillService _skillService;
         private static MailHelper _mailHelper;
         public EmailController(ILogger<EmailController> logger, MailHelper mailHelper)
         {
