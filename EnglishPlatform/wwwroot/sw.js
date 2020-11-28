0920c5c9413c8fc304bf2bb97c747389477132fc
VietPhung <vietphung.it@gmail.com>
2020-11-28 12:03:27 +0700

    check in sw.js

---
+++ EnglishPlatform/wwwroot/sw.js
@@ -0,0 +1,15 @@
+﻿var __PUBLIC_KEY = "BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk";
+var __PRIVATE_KEY = "ERIZmc5T5uWGeRxedxu92k3HnpVwy_RCnQfgek1x2Y4";
+
+var urlB64ToUint8Array = base64String => {
+    var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, "+")
+        .replace(/_/g, "/");
+    var rawData = atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+};
